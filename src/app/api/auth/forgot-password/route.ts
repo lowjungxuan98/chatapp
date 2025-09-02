17e0594b0268d249b8e698b5c16f3c32534015e5
lowjungxuan <lowjungxuan@gmail.com>
2025-09-02 22:24:31 +0800

    refactor: Update type annotations from String to string in authentication API routes

--- src/app/api/auth/forgot-password/route.ts
+++ src/app/api/auth/forgot-password/route.ts
@@ -4,25 +4,25 @@ import { RouteMiddleware } from './middleware';
 import { forgotPassword } from './service';
 import ApiError from '@/app/api/type';
 
-export const POST = RouteMiddleware<String>(async (request: NextRequest) => {
+export const POST = RouteMiddleware<string>(async (request: NextRequest) => {
   try {
     const body = await request.json();
     const resetToken = await forgotPassword(body.email);
-    return NextResponse.json<ApiResponse<String>>({
+    return NextResponse.json<ApiResponse<string>>({
       success: true,
       message: 'Password reset email sent successfully',
       data: resetToken
     }, { status: 200 });
   } catch (error) {
     if (error instanceof ApiError) {
-      return NextResponse.json<ApiResponse<String>>({
+      return NextResponse.json<ApiResponse<string>>({
         success: false,
         message: error.message,
         error: error
       }, { status: error.statusCode });
     }
     
-    return NextResponse.json<ApiResponse<String>>({
+    return NextResponse.json<ApiResponse<string>>({
       success: false,
       message: 'Failed to process password reset request',
       error: error
--- src/app/api/auth/send-verification-email/route.ts
+++ src/app/api/auth/send-verification-email/route.ts
@@ -6,27 +6,27 @@ import { generateVerifyEmailToken } from '@/app/api/jwt';
 import { sendVerificationEmail } from '@/app/api/email';
 import httpStatus from 'http-status';
 
-export const POST = RouteMiddleware<String>(async (request: NextRequest) => {
+export const POST = RouteMiddleware<string>(async (request: NextRequest) => {
     try {
         const user = request.user;
         if (!user) throw new ApiError(httpStatus.UNAUTHORIZED, 'Unauthorized');
         const token = await generateVerifyEmailToken(user);
         await sendVerificationEmail(user.email, token);
-        return NextResponse.json<ApiResponse<String>>({
+        return NextResponse.json<ApiResponse<string>>({
             success: true,
             message: 'Verification email sent',
             data: token
         }, { status: 200 });
     } catch (error) {
         if (error instanceof ApiError) {
-            return NextResponse.json<ApiResponse<String>>({
+            return NextResponse.json<ApiResponse<string>>({
                 success: false,
                 message: error.message,
                 error: error
             }, { status: error.statusCode });
         }
 
-        return NextResponse.json<ApiResponse<String>>({
+        return NextResponse.json<ApiResponse<string>>({
             success: false,
             message: 'Failed to send verification email',
             error: error
